41e9c194ec5f57d5048a890663208ce8e92d5d83
Susil-Commits <Susil@2004>
2026-08-03 19:31:40 +0530

    fix: update URL for Full Stack Engineer resume

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -31,7 +31,7 @@ export const personal = {
     {
       role: 'Full Stack Engineer',
       subtitle: 'Development',
-      url: 'https://drive.google.com/file/d/1OLdNqlmIcZIPzEZsQkFQba23RJSVo0SJ/view?usp=sharing',
+      url: 'https://drive.google.com/file/d/1xOvRMl95HpRyGOWGeC2nAc_WqCpPBUKK/view?usp=sharing',
       icon: 'mern',
     },
     {
